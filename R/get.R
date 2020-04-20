bb9c912884185be31076769f15766a9696641b51
John Blischak <john.blischak@abbvie.com>
2020-04-20 11:10:02 -0400

    Create S3 generic to get inferences from study.

--- R/get.R
+++ R/get.R
@@ -49,3 +49,65 @@ getModels.character <- function(study, model = NULL, libraries = NULL) {
 getModels.default <- function(study, model = NULL) {
   stop(sprintf("No method for object of class \"%s\"", class(study)))
 }
+
+#' @export
+getInferences <- function(study, model = NULL, contrast = NULL) {
+  UseMethod("getInferences")
+}
+
+#' @export
+getInferences.oaStudy <- function(study, model = NULL, contrast = NULL) {
+  if (is.null(study[["inferences"]])) {
+    stop(sprintf("No Inferences available for study \"%s\"", study[["name"]]))
+  }
+
+  return(study[["inferences"]])
+}
+
+#' @export
+getInferences.SQLiteConnection <- function(study, model = NULL, contrast = NULL) {
+
+  df_inferences <- study %>%
+    dplyr::tbl("inferences") %>%
+    dplyr::collect()
+
+  inferences <- splitTableIntoList(df_inferences, "modelID")
+  inferences <- lapply(inferences, function(x) splitTableIntoList(x, "contrastID"))
+
+  return(inferences)
+}
+
+#' @export
+getInferences.character <- function(study, model = NULL, contrast = NULL, libraries = NULL) {
+
+  pkg <- paste0("OAstudy", study)
+  location <- find.package(pkg, lib.loc = libraries, quiet = TRUE)
+  if (length(location) == 0) {
+    stop(sprintf("Unable to find study package for \"%s\"", study))
+  }
+
+  dbName <- file.path("OmicAnalyzer", paste0(study, ".sqlite"))
+  db <- system.file(dbName, package = pkg, mustWork = TRUE)
+
+  con <- DBI::dbConnect(RSQLite::SQLite(), db)
+  on.exit(DBI::dbDisconnect(con))
+
+  inferences <- getInferences(con, model = model, contrast = contrast)
+
+  return(inferences)
+}
+
+#' @export
+getInferences.default <- function(study, model = NULL, contrast = NULL) {
+  stop(sprintf("No method for object of class \"%s\"", class(study)))
+}
+
+# Wrapper around base::split()
+splitTableIntoList <- function(dataFrame, columnName) {
+
+  splitVariable <- dataFrame[[columnName]]
+  splitData <- dataFrame
+  splitData[[columnName]] <- NULL
+
+  return(split(splitData, splitVariable))
+}
