abf25a56b12250a4a9a3c0b2ce2e2ce1aa084e5b
John Blischak <john.blischak@abbvie.com>
2020-12-21 21:04:32 -0500

    Proof of concept to download app during installation.

---
+++ inst/install-app.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+
+# Proof of concept for downloading zipfile to R package during installation
+
+installDir <- commandArgs(trailingOnly = TRUE)
+zipurl <- "https://github.com/jdblischak/workflowr/archive/v1.6.2.zip"
+zipfile <- file.path(installDir, "build.zip")
+utils::download.file(url = zipurl, destfile = zipfile)
+utils::unzip(zipfile, exdir = installDir)
