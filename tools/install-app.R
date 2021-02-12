a01ae1ecaf2495ed24f15140c8ca0ab25133bf7f
John Blischak <john.blischak@abbvie.com>
2021-02-12 10:08:56 -0500

    Download zipfile from OmicNavigatorWebApp release.

--- tools/install-app.R
+++ tools/install-app.R
@@ -1,10 +1,14 @@
 #!/usr/bin/env Rscript
 
-# Proof of concept for downloading zipfile to R package during installation
+# Download the web app zipfile during R package installation
 
 installDir <- commandArgs(trailingOnly = TRUE)
-zipurl <- "https://github.com/jdblischak/workflowr/archive/v1.6.2.zip"
+zipurl <- "https://github.com/AbbVie-External/OmicNavigatorWebApp/releases/download/v1.0.0/build.zip"
 zipfile <- file.path(installDir, "build.zip")
 utils::download.file(url = zipurl, destfile = zipfile)
 utils::unzip(zipfile, exdir = installDir)
 unlink(zipfile)
+# Change name to www/
+dirBuild <- file.path(installDir, "build")
+dirWww <- file.path(installDir, "www")
+file.rename(dirBuild, dirWww)
