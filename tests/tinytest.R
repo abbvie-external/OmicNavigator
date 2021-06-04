468887136dbe12e7eb8ef302962e21a7f91b95c3
John Blischak <john.blischak@abbvie.com>
2021-06-04 10:35:42 -0400

    Suppress messages from tests run by `R CMD check`

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,4 +1,4 @@
 # Test files in inst/tinytest/
 if (requireNamespace("tinytest", quietly = TRUE)) {
-  tinytest::test_package("OmicNavigator")
+  suppressMessages(tinytest::test_package("OmicNavigator"))
 }
