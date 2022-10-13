d079f6b96ffb7144cdb68ad82f023b3ae7c94c0a
John Blischak <john.blischak@abbvie.com>
2022-10-13 12:05:45 -0400

    Manually install RSPM binaries on Ubuntu and Windows
    The setup-r action is working for faviconPlease, but not for
    OmicNavigator for some reason

--- scripts/install-dependencies-dev.R
+++ scripts/install-dependencies-dev.R
@@ -2,9 +2,15 @@
 
 # Used by GitHub Action workflow release.yml
 
-# Use RSPM to install package binaries
+# Use RSPM to install package binaries (Linux, Windows)
 options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))
-options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/__linux__/focal/2022-02-07+Y3JhbiwyOjQ1MjYyMTU7MjBCMTdBQ0M"))
+os <- Sys.info()["sysname"]
+if (os == "Linux") {
+  options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/__linux__/focal/2022-02-07+Y3JhbiwyOjQ1MjYyMTU7MjBCMTdBQ0M"))
+}
+if (os == "Windows") {
+  options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/2022-02-07+Y3JhbiwyOjQ1MjYyMTU7MjBCMTdBQ0M"))
+}
 
 devPkgs <- c(
   "commonmark", # for utils::news() to create release notes
--- scripts/install-dependencies.R
+++ scripts/install-dependencies.R
@@ -1,6 +1,12 @@
-# Use RSPM to install package binaries
+# Use RSPM to install package binaries (Linux, Windows)
 options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))
-options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/__linux__/focal/2022-02-07+Y3JhbiwyOjQ1MjYyMTU7MjBCMTdBQ0M"))
+os <- Sys.info()["sysname"]
+if (os == "Linux") {
+  options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/__linux__/focal/2022-02-07+Y3JhbiwyOjQ1MjYyMTU7MjBCMTdBQ0M"))
+}
+if (os == "Windows") {
+  options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/2022-02-07+Y3JhbiwyOjQ1MjYyMTU7MjBCMTdBQ0M"))
+}
 
 if (!requireNamespace("remotes", quietly = TRUE)) {
   install.packages("remotes")
