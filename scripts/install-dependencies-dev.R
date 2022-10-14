a7d29f078182b0b5460e56482aa003c6e5676752
John Blischak <john.blischak@abbvie.com>
2022-10-14 10:29:52 -0400

    Bump RSPM freeze date to 2022-10-13
    https://packagemanager.rstudio.com/client/#/repos/1/overview
    https://packagemanager.rstudio.com/all/2022-10-13+Y3JhbiwyOjQ1MjYyMTU7RjQxMTNEM0U

--- scripts/install-dependencies-dev.R
+++ scripts/install-dependencies-dev.R
@@ -6,10 +6,10 @@
 options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))
 os <- Sys.info()["sysname"]
 if (os == "Linux") {
-  options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/__linux__/focal/2022-02-07+Y3JhbiwyOjQ1MjYyMTU7MjBCMTdBQ0M"))
+  options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/__linux__/focal/2022-10-13+Y3JhbiwyOjQ1MjYyMTU7RjQxMTNEM0U"))
 }
 if (os == "Windows") {
-  options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/2022-02-07+Y3JhbiwyOjQ1MjYyMTU7MjBCMTdBQ0M"))
+  options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/2022-10-13+Y3JhbiwyOjQ1MjYyMTU7RjQxMTNEM0U"))
 }
 
 devPkgs <- c(
