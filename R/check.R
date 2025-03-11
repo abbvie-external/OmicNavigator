9a71d52c77f86155328cae00aa9a7d7552db7da7
GEOFFEC1 <elyse.geoffroy@abbvie.com>
2025-03-11 07:37:31 -0500

    check length of terms

--- R/check.R
+++ R/check.R
@@ -305,7 +305,7 @@ checkAnnotations <- function(annotations) {
     }
     terms <- annotations[[i]][["terms"]]
     checkList(terms, allowEmpty = FALSE)
-    if (!all(vapply(terms, is.character, logical(1))) || any(vapply(terms, isEmpty, logical(1)))) {
+    if (!all(vapply(terms, is.character, logical(1))) || !!length(terms)) {
       stop(sprintf("The terms for \"%s\" must be a named list of character vectors",
                    annotationID))
     }
