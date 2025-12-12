58b94b2b69e5c80eee4b7f76ab83afd5814e3e10
John Blischak <jdblischak@gmail.com>
2025-12-12 15:23:04 -0500

    Update sanitizeAssays() and start tests for multiple transformations

--- R/sanitize.R
+++ R/sanitize.R
@@ -24,7 +24,14 @@ sanitizeModels <- function(models) {
 
 sanitizeAssays <- function(assays) {
   for (i in seq_along(assays)) {
-    assays[[i]] <- as.data.frame(assays[[i]])
+    if (is.list(assays[[i]]) && !is.data.frame(assays[[i]])) {
+      # support multiple transformations
+      for (j in seq_along(assays[[i]])) {
+        assays[[i]][[j]] <- as.data.frame(assays[[i]][[j]])
+      }
+    } else {
+      assays[[i]] <- as.data.frame(assays[[i]])
+    }
   }
 
   return(assays)
---
+++ inst/tinytest/testTransformations.R
@@ -0,0 +1,86 @@
+# Test support for multiple transformations for assays and metaAssays
+
+# Setup ------------------------------------------------------------------------
+
+source("tinytestSettings.R")
+using(ttdo)
+
+library(OmicNavigator)
+
+tmplib <- tempfile()
+dir.create(tmplib)
+
+testStudyName <- "transformations"
+testStudyObj <- OmicNavigator:::testStudy(testStudyName)
+
+assaysDataFrame <- OmicNavigator:::testAssays(n = 1)[[1]]
+assaysWithTransformations <- list(
+  model_01 = assaysDataFrame,
+  model_02 = list(a1 = assaysDataFrame, a2 = assaysDataFrame + 1)
+)
+
+# checkAssays() ----------------------------------------------------------------
+
+expect_silent_xl(
+  OmicNavigator:::checkAssays(assays = assaysWithTransformations)
+)
+
+expect_error_xl(
+  OmicNavigator:::checkAssays(assays = c(assaysWithTransformations, model_04 = "banana"))
+)
+
+expect_error_xl(
+  OmicNavigator:::checkAssays(
+    assays = c(
+      assaysWithTransformations,
+      model_04 = list(a1 = cbind(assaysDataFrame, "banana"), a2 = assaysDataFrame + 1)
+    )
+  ),
+  "The columns of the assays data frame must all be numeric.*model_04"
+)
+
+# sanitizeAssays() -------------------------------------------------------------
+
+assaysClasses <- assaysWithTransformations
+# Brute-forcing this one-time operation instead of doing something more complex
+class(assaysClasses[["model_01"]]) <- c("custom_class", "data.frame")
+class(assaysClasses[["model_02"]][["a1"]]) <- c("custom_class", "data.frame")
+class(assaysClasses[["model_02"]][["a2"]]) <- c("custom_class", "data.frame")
+
+expect_identical_xl(
+  OmicNavigator:::sanitizeAssays(assaysClasses),
+  assaysWithTransformations
+)
+
+# addAssays() ------------------------------------------------------------------
+
+expect_silent_xl(
+  testStudyObj <- addAssays(
+    testStudyObj,
+    assays = list(
+      model_04 = list(
+        a1 = assaysDataFrame,
+        a2 = assaysDataFrame + 1
+      )
+    )
+  )
+)
+
+# getAssays() ------------------------------------------------------------------
+
+expect_identical_xl(
+  getAssays(testStudyObj),
+  testStudyObj[["assays"]]
+)
+
+expect_identical_xl(
+  getAssays(testStudyObj, modelID = "model_04"),
+  list(
+    a1 = assaysDataFrame,
+    a2 = assaysDataFrame + 1
+  )
+)
+
+# Teardown ---------------------------------------------------------------------
+
+unlink(tmplib, recursive = TRUE, force = TRUE)
